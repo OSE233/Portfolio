e6fdc0ac0895d8ae7f072c4440a8a7b8e619ed87
OSE233 <semilore.olatunji123@gmail.com>
2026-08-29 13:15:42 +0100

    Renamed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import NotFound from "./Pages/NotFound";
 import Profile from "./Components/Profile";
 import NavBar from "./Components/NavBar";
 import YtLinks from "./Components/YtLinks";
-
-import Mobileprojects from "./Components/Mobileprojects";
+import Mobileprojects from "./Components/MobileProjects";
 import Webprojects from "./Components/WebProjects";
 
 
